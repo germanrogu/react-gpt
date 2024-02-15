3d4d16f9f368dcbec1f0e57dbc9c16c25fce1f2e
germanrogu <german.rodriguezgu@gmail.com>
2024-02-14 21:17:45 -0500

    Children routes

--- src/presentation/router/router.tsx
+++ src/presentation/router/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import {
   OrthographyPage,
   ProsConsPage,
@@ -82,6 +82,15 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <DashboardLayout />,
-    children: [],
+    children: [
+      ...menuRoutes.map((route) => ({
+        path: route.to,
+        element: route.component,
+      })),
+      {
+        path: "",
+        element: <Navigate to={menuRoutes[0].to} />,
+      },
+    ],
   },
 ]);
