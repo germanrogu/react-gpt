1cac9f8f64ca2cb80a091617d2f1c8b0a69c2fc2
germanrogu <german.rodriguezgu@gmail.com>
2024-02-18 13:35:18 -0500

    OrthographyPage render message, loader TextMessage Box defaults

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components";
 
 export const OrthographyPage = () => {
-  return <div>OrthographyPage</div>;
+  return (
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+          <GptMessage text='Hola, puedes escribir tu texto en espanol y te ayudo con las correciones' />
+
+          <MyMessage text='Hola mundo' />
+
+          <TypingLoader className='fade-in' />
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={(message) => console.log(message)}
+        placeholder='Escribe lo que quieras'
+        disableCorrections
+      />
+    </div>
+  );
 };
